586cafda0930565506a4e0ed4dac06b2e710fa50
Rhendy Diki N <rendydikinugraha@gmail.com>
2022-12-28 22:35:47 +0700

    File Python

---
+++ Latihan1.py
@@ -0,0 +1,23 @@
+txt = 'Hello World'
+
+
+# Mengganti karakter H dengan karakter J
+#print(txt.replace("H", "J"))
+
+
+#Mengubah huruf menjadi besar
+#print(txt.upper())
+#Mengubah huruf menjadi kecil
+#print(txt.lower())
+
+
+#Menghapus spasi dara text tersebut
+#print(txt.replace(" ", ""))
+
+
+#Mengambil karakter index 2 sampai index 4
+#print(txt[2:5])
+
+
+#Untuk menghitung jumlah dari karakter diatas
+#print(len(txt))
---
+++ Latihan2.py
@@ -0,0 +1,4 @@
+umur = 24
+txt = 'Hello, nama saya john, dan umur saya adalah {0} tahun'
+
+print(txt.format(umur))
